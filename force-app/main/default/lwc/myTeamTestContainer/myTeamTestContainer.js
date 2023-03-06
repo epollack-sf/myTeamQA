4b2b8fd2136a472eedaff8d62aa071cf9d597b64
Evan Pollack <epollack@salesforce.com>
2023-03-06 17:48:35 -0500

    currently working on getting and formatting user data

--- force-app/main/default/lwc/myTeamTestContainer/myTeamTestContainer.js
+++ force-app/main/default/lwc/myTeamTestContainer/myTeamTestContainer.js
@@ -1,7 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getSkillsForUser from '@salesforce/apex/testController.getSkillsForUser';
 
 export default class MyTeamTestContainer extends LightningElement {
-    skills = [
+    
+    // should be api (pulled from parent component or LMS)
+    userIdServices = '0053A00000FGfNiQAL';
+
+    // should be api (pulled from parent component or LMS)
+    employee = {
+        FirstName: 'Ayesha',
+        LastName: 'Singh',
+        get FullName() {
+            return this.FirstName + this.LastName
+        }
+    };
+
+    skills;
+
+    @wire(getSkillsForUser, { userId: '$userIdServices' })
+    wiredSkillData(value) {
+        if (value.data) {
+            this.skills = value.data;
+        }
+
+        if (value.error) {
+            console.warn(value.error);
+        }
+    }
+    
+    /*skills = [
         {
             id: '1',
             Type__c: 'Type',
@@ -26,7 +53,7 @@ export default class MyTeamTestContainer extends LightningElement {
             Category__c: 'Skiing',
             Rating__c: 2
         },
-    ];
+    ];*/
 
     skills2 = [
         {
@@ -103,9 +130,9 @@ export default class MyTeamTestContainer extends LightningElement {
         },
     ];
 
-    employee = {
+    /*employee = {
         Name: 'Evan'
-    };
+    };*/
 
     employee2 = {
         Name: 'Claude'
